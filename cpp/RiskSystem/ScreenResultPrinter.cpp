c76e3d1ff257309e902693113248d36862f4392c
Spencerjibz <spencernajib2@gmail.com>
2025-12-18 19:48:54 +0000

    Exercise 6

--- cpp/RiskSystem/ScreenResultPrinter.cpp
+++ cpp/RiskSystem/ScreenResultPrinter.cpp
@@ -1,13 +1,27 @@
 #include "ScreenResultPrinter.h"
+#include "optional"
 #include <iostream>
+#include <ostream>
+#include <sstream>
+#include <string>
 
+#include <string>
 void ScreenResultPrinter::printResults(ScalarResults &results) {
-  for (const auto &result : results) {
-    // Write code here to print out the results such that we have:
-    // TradeID : Result : Error
-    // If there is no result then the output should be:
-    // TradeID : Error
-    // If there is no error the output should be:
-    // TradeID : Result
-  }
+    for (const auto &result : results) {
+        auto tradeId = result.getTradeId();
+        std::optional<double> expected_result = result.getResult();
+        std::optional<std::string> error = result.getError();
+        std::stringstream stream;
+        stream << tradeId;
+        if (expected_result.has_value()) {
+            stream << " : ";
+            stream << expected_result.value();
+        }
+
+        if (error.has_value()) {
+            stream << " : ";
+            stream << error.value();
+        }
+        std::cout << stream.str() << std::endl;
+    }
 }
